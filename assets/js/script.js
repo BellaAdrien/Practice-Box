62174c3bc3a3ab83dca5e9947b21772fc411b159
BellaAdrien <Iadrien2020@fau.edu>
2023-10-31 11:59:51 -0400

    saveport

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,29 @@
+var introEl = document.querySelector("#intro-section");
+var navEl = document.querySelector("#navigation");
+var favoriteEl = document.querySelector("#favorite-section");
+var nasaEL = document.querySelector("#nasa-photo");
+var navHome = document.querySelector("#home");
+var navFave = document.querySelector("#favorite");
+var userName = document.querySelector("#user-name");
+var nameEl = document.querySelectorAll(".name");
+var favoriteBtn = document.querySelector("#favorite-button");
+var favContainerEl = document.querySelector(".fav-container");
 
+function goHome() {
+    nasaEL.classList.add("hide");
+    favoriteEl.classList.add("hide");
+    navEl.classList.add("hide");
+    introEl.removeAttribute("class");
 
+}
+function goFave() {
+    nasaEL.classList.add("hide");
+    introEl.classList.add("hide");
+    navEl.removeAttribute("class");
+    favoriteEl.classList.remove("hide");
+    getFields()
 
+}
 
 function addClass() {
     var element = document.getElementById("card-info");
@@ -11,3 +34,7 @@ function addClass() {
     var element = document.getElementById("card-info");
     element.classList.remove("data-show");
   }
+
+
+  navHome.addEventListener("click", goHome);
+navFave.addEventListener("click", goFave);
