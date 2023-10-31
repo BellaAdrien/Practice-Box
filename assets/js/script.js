a0ff3b8c2cb33d5480770cbbb781a30c0d2e3da1
BellaAdrien <Iadrien2020@fau.edu>
2023-10-31 11:57:58 -0400

    saveport

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+
+
+
+
+function addClass() {
+    var element = document.getElementById("card-info");
+    element.classList.add("data-show");
+  }
+  
+  function removeClass() {
+    var element = document.getElementById("card-info");
+    element.classList.remove("data-show");
+  }
